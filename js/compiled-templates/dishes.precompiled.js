4bc946b18c3ca41a32172417b3c912b2d65de13a
Sai <sai.g@1digitals.com>
2021-01-24 13:26:50 +0530

    Dynamic menu & bestsellers implementation

---
+++ js/compiled-templates/dishes.precompiled.js
@@ -0,0 +1,35 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['dishes'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"col-sm-3 col-md-6 col-lg-3\">\r\n    <div class=\"card\">\r\n        <img class=\"card-img-top\" src=\"./assets/dishes/"
+    + alias4(((helper = (helper = lookupProperty(helpers,"code") || (depth0 != null ? lookupProperty(depth0,"code") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"code","hash":{},"data":data,"loc":{"start":{"line":4,"column":55},"end":{"line":4,"column":63}}}) : helper)))
+    + ".jpg\" alt=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":4,"column":74},"end":{"line":4,"column":82}}}) : helper)))
+    + "\">\r\n        <div class=\"card-body\">\r\n            <h5 class=\"card-title\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":6,"column":35},"end":{"line":6,"column":43}}}) : helper)))
+    + "</h5>\r\n            <p class=\"card-text dish-short-description\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"description") || (depth0 != null ? lookupProperty(depth0,"description") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data,"loc":{"start":{"line":7,"column":56},"end":{"line":7,"column":71}}}) : helper)))
+    + "</p>\r\n            <p class=\"card-text dish-price\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"currencySymbol") || (depth0 != null ? lookupProperty(depth0,"currencySymbol") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"currencySymbol","hash":{},"data":data,"loc":{"start":{"line":8,"column":44},"end":{"line":8,"column":62}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"price") || (depth0 != null ? lookupProperty(depth0,"price") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data,"loc":{"start":{"line":8,"column":62},"end":{"line":8,"column":71}}}) : helper)))
+    + "</p>\r\n            <a href=\"#\" class=\"btn btn-primary\" onclick=\"add("
+    + alias4(((helper = (helper = lookupProperty(helpers,"code") || (depth0 != null ? lookupProperty(depth0,"code") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"code","hash":{},"data":data,"loc":{"start":{"line":9,"column":61},"end":{"line":9,"column":69}}}) : helper)))
+    + ")\">Add</a>\r\n        </div>\r\n    </div>\r\n</div>\r\n";
+},"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"items") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":1,"column":0},"end":{"line":13,"column":9}}})) != null ? stack1 : "");
+},"useData":true});
+})();
---
+++ js/homepage.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+    var template = Handlebars.templates.dishes;
+    getBestSellers().then(function(bestSellers){
+        $('#bestsellers').html(template(bestSellers));
+    })
+});
+
+async function getBestSellers(){
+let menu = await $.get("/data/menu.json",function(response){
+    return response
+})
+let bestSellers = menu.items.filter(item=>item.isBestSeller==true)
+bestSellers = {items:bestSellers};
+return bestSellers;
+
+
+}
